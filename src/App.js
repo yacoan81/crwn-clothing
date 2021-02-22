c91524c503ad863fe94df39e9dbdbdf3ddae0d1c
qc25pk <javier.rodriguez.soto@ing.com>
2021-02-22 13:26:52 +0100

    bababa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import { BrowserRouter, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+  <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
