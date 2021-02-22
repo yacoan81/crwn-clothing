906f2fc3d17e5f9be6aa89c26ed48e2c5c531067
qc25pk <javier.rodriguez.soto@ing.com>
2021-02-22 16:52:56 +0100

    create menus

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import { BrowserRouter, Switch } from "react-router-dom";
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-  <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>    
     </div>
-    
   );
 }
 
